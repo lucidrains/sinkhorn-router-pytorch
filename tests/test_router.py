7ce5fae070807c0bb8db17be8f5513384502f11b
Phil Wang <lucidrains@gmail.com>
2024-08-26 07:31:08 -0700

    make sure masking always runs

--- tests/test_router.py
+++ tests/test_router.py
@@ -6,18 +6,25 @@ from sinkhorn_router_pytorch import SinkhornRouter
 
 @pytest.mark.parametrize('competitive,causal', [(True, False), (False, True), (False, False)])
 @pytest.mark.parametrize('seq_len', (1, 77, 1024, 1999))
+@pytest.mark.parametrize('experts_as_tensor', (True, False))
+@pytest.mark.parametrize('has_masking', (True, False))
 def test_moe(
     competitive,
     causal,
-    seq_len
+    seq_len,
+    experts_as_tensor,
+    has_masking
 ):
 
-    experts = [
-        nn.Linear(512, 512),
-        nn.Linear(512, 512),
-        nn.Linear(512, 512),
-        nn.Linear(512, 512),
-    ]
+    if experts_as_tensor:
+        experts = nn.Parameter(torch.randn(4, 512, 512))
+    else:
+        experts = [
+            nn.Linear(512, 512),
+            nn.Linear(512, 512),
+            nn.Linear(512, 512),
+            nn.Linear(512, 512),
+        ]
 
     router = SinkhornRouter(
         dim = 512,
@@ -29,16 +36,23 @@ def test_moe(
     assert router.num_experts == 4
 
     x = torch.randn(2, seq_len, 512)
-    out = router(x)
+
+    mask = None
+    if has_masking:
+        mask = torch.randint(0, 2, (2, seq_len)).bool()
+
+    out = router(x, mask = mask)
 
     assert out.shape == (2, seq_len, 512)
 
 @pytest.mark.parametrize('competitive,causal', [(True, False), (False, True), (False, False)])
 @pytest.mark.parametrize('seq_len', (1, 77, 1024, 1999))
+@pytest.mark.parametrize('has_masking', (True, False))
 def test_multiheaded_experts(
     competitive,
     causal,
-    seq_len
+    seq_len,
+    has_masking
 ):
 
     experts = nn.Parameter(torch.randn(16, 8, 512, 256)) # (experts, heads, dim [in], dim [out])
@@ -53,6 +67,11 @@ def test_multiheaded_experts(
     assert router.num_experts == 16
 
     x = torch.randn(2, 8, seq_len, 512)
-    out = router(x)
+
+    mask = None
+    if has_masking:
+        mask = torch.randint(0, 2, (2, seq_len)).bool()
+
+    out = router(x, mask = mask)
 
     assert out.shape == (2, 8, seq_len, 256)
